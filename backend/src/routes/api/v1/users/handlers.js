ab9e2133571f987b33b5d8300eb55e5d130bd44f
ShaoYunMa <j8832266@gmail.com>
2023-04-19 02:50:17 +0800

    add database

--- backend/src/routes/api/v1/users/handlers.js
+++ backend/src/routes/api/v1/users/handlers.js
@@ -1,3 +1,9 @@
+import { prisma } from "../../../../adapters";
+export async function getAllUsers(req, res) {
+    const allUsers = await prisma.user.findMany();
+    return res.json(allUsers);
+}
+/*
 export async function getAllUsers(req, res) {
     return res.json([
         {
@@ -7,3 +13,4 @@ export async function getAllUsers(req, res) {
         },
     ]);
 }
+*/
